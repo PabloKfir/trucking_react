9543f5abc02b0e2f01419cf25794212ec542440e
Pablo Garcia <pablogarcialgc@gmail.com>
2023-01-13 16:23:21 -0300

    Some css changes

--- src/compare/compareList.js
+++ src/compare/compareList.js
@@ -12,7 +12,7 @@ return(
     <li className='compare__container'>
         <div className='compare__body'>{props.brand}</div>
         <div className='compare__body'>{props.model}</div>
-        <div className='compare__body'>{props.img}</div>
+        <img className='compare__body' src={props.img} alt='img'></img>
         <div className='compare__body'>{props.market.map((market, index)=>{ return(<div key={index}>{market}</div>)})}</div>
         <div className='compare__body'>{props.cab.map((cab, index)=>{ return(<div key={index}>{cab}</div>)})}</div>
         <div className='compare__body'>{props.engine.map((engine, index)=>{ return(<div key={index}>{engine}</div>)})}</div>
